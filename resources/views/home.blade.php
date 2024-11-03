ee953768b97fde6b1bf9ec48b4c85d0aa4d143c0
Haris Rahman <iharisrahman@yahoo.com>
2024-11-04 01:24:28 +0530

    init

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel 9 Playground</title>
+</head>
+
+<body>
+    <h1 class="text-5xl font-bold underline text-orange-500">Laravel 9 Playground</h1>
+
+
+    @vite(['resources/css/app.scss', 'resources/js/app.js'])
+</body>
+
+</html>
